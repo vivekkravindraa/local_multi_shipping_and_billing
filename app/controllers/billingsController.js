6e9372bb6422dafbcb34415605ec4c87e70454f9
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-09-28 16:20:55 +0530

    Billing Schema & Routes

--- app/controllers/billingsController.js
+++ app/controllers/billingsController.js
@@ -4,7 +4,34 @@ const router = express.Router();
 
 const { Billing } =  require('../models/Billing');
 
-router.get('/', (req,res) => { });
-router.post('/', (req,res) => { });
+router.get('/', (req,res) => {
+    Billing.find()
+    .then((response) => {
+        if(response) {
+            res.status(200).send(response);
+        } else {
+            res.status(404).send('Billings data not found!');
+        }
+    })
+    .catch((e) => {
+        res.status(400).send('Unable to fetch the details..');
+    })
+});
+
+router.get('/:billingId', (req,res) => {
+    let id = req.params.billingId;
+
+    Billing.findOne({ id: billingId })
+    .then((response) => {
+        if(response) {
+            res.status(200).send(response);
+        } else {
+            res.status(404).send('Billing data not found!');
+        }
+    })
+    .catch((e) => {
+        res.status(400).send('Unable to fetch the details..');
+    })
+});
 
 module.exports = { billingsController: router }
