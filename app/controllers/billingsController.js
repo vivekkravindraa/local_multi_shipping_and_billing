8b21d8b53c6fa878f97e813d629ac14ea924767e
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-09-30 19:20:58 +0530

    Set Status To Active

--- app/controllers/billingsController.js
+++ app/controllers/billingsController.js
@@ -30,8 +30,69 @@ router.get('/id', (req,res) => {
     Billing.findOne({ 'recurring_application_charge.id': id })
     .then((billing) => {
         if(billing) {
-            console.log('BILLING RESPONSE', billing);
-            res.status(200).redirect(billing.recurring_application_charge.confirmation_url);
+            let billingBody = {
+                'recurring_application_charge': {
+                    'id': Number(`${billing.recurring_application_charge.id}`),
+                    'name': `${billing.recurring_application_charge.name}`,
+                    'api_client_id': Number(`${billing.recurring_application_charge.api_client_id}`),
+                    'price': `${billing.recurring_application_charge.price}`,
+                    'status': `${billing.recurring_application_charge.status}` == 'pending' ? 'accepted' : `${billing.recurring_application_charge.status}`,
+                    'return_url': `${billing.recurring_application_charge.return_url}`,
+                    'billing_on': `${billing.recurring_application_charge.created_at}` == null ? `${billing.recurring_application_charge.created_at.slice(0,10)}` : `${billing.recurring_application_charge.created_at}`,
+                    'created_at': `${billing.recurring_application_charge.created_at}`,
+                    'updated_at': `${billing.recurring_application_charge.updated_at}`,
+                    'test': Boolean(`${billing.recurring_application_charge.test}`),
+                    'activated_on': `${billing.recurring_application_charge.activated_on}` == 'null' ? null : `${billing.recurring_application_charge.activated_on}`,
+                    'cancelled_on': `${billing.recurring_application_charge.cancelled_on}` == 'null' ? null : `${billing.recurring_application_charge.cancelled_on}`,
+                    'trial_days': Number(`${billing.recurring_application_charge.trial_days}`),
+                    'trail_ends_on': `${billing.recurring_application_charge.trail_ends_on}` == 'null' ? null : `${billing.recurring_application_charge.trail_ends_on}`,
+                    'decorated_return_url': `${billing.recurring_application_charge.decorated_return_url}`
+                }
+            };
+
+            Shopify.findOne({ shopDomain: billing.shopName })
+            .then((response) => {
+                let shop = response.shopDomain;
+                let token = response.accessToken;
+                
+                axios
+                    .post(`https://${shop}/admin/api/${apiVersion}/recurring_application_charges/${id}/activate.json`,
+                        billingBody,
+                        { headers: { 'X-Shopify-Access-Token' : token }
+                    })
+                    .then((response) => {
+                        if(response) {
+                            Billing.findOne({ 'recurring_application.charge.id': response.recurring_application_charge.id })
+                            .then((response) => {
+                                if(response) {
+                                    let objectId = response._id;
+                                    let body = {
+                                        'recurring_application_charge': {
+                                            'status': response.recurring_application_charge.status,
+                                            'billing_on': response.recurring_application_charge.billing_on
+                                        }
+                                    }
+                                    Billing.findByIdAndUpdate(objectId, { $set: body }, { new: true })
+                                    .then((response) => {
+                                        console.log('UPDATED BILLING BODY', response);
+                                    })
+                                    .catch((e) => {
+                                        console.log(e);
+                                    })
+                                }
+                            })
+                            .catch((e) => {
+                                console.log(e);
+                            })
+                        }
+                    })
+                    .catch((e) => {
+                        console.log(e);
+                    })
+            })
+            .catch((e) => {
+                console.log(e);
+            })
         } else {
             res.status(404).send('Billing data not found!');
         }
