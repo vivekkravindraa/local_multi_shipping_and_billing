546088ed6f1849acc6d9d9732ba5a7217d606d78
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-09-30 17:02:18 +0530

    Billing Controllers

--- app/controllers/billingsController.js
+++ app/controllers/billingsController.js
@@ -30,46 +30,8 @@ router.get('/id', (req,res) => {
     Billing.findOne({ 'recurring_application_charge.id': id })
     .then((billing) => {
         if(billing) {
-            // res.status(200).send(billing);
             console.log('BILLING RESPONSE', billing);
-            let billingBody = {
-                'recurring_application_charge': {
-                    'id': `${billing.recurring_application_charge.id}`,
-                    'name': `${billing.recurring_application_charge.name}`,
-                    'api_client_id': `${billing.recurring_application_charge.api_client_id}`,
-                    'price': `${billing.recurring_application_charge.price}`,
-                    'status': `${billing.recurring_application_charge.status}` == 'pending' ? 'accepted' : `${billing.recurring_application_charge.status}`,
-                    'return_url': `${billing.recurring_application_charge.return_url}`,
-                    'billing_on': `${billing.recurring_application_charge.created_at.slice(0,10)}`,
-                    'created_at': `${billing.recurring_application_charge.created_at}`,
-                    'updated_at': `${billing.recurring_application_charge.updated_at}`,
-                    'test': `${billing.recurring_application_charge.test}`,
-                    'activated_on': `${billing.recurring_application_charge.activated_on}`,
-                    'cancelled_on': `${billing.recurring_application_charge.cancelled_on}`,
-                    'trial_days': `${billing.recurring_application_charge.trial_days}`,
-                    'trail_ends_on': `${billing.recurring_application_charge.trail_ends_on}`,
-                    'decorated_return_url': `${billing.recurring_application_charge.decorated_return_url}`
-                }
-            };
-            let shop = billing.shopName;
-            let chargeId = billing.recurring_application_charge.id;
-            Shopify.findOne({ shopDomain: shop }).then((shopResponse) => {
-                if(shopResponse) {
-                    console.log('SHOP RESPONSE', shopResponse);
-                    let shopDomain = shopResponse.shopDomain;
-                    let accessToken = shopResponse.accessToken;
-                    axios
-                        .post(`https://${shopDomain}/admin/api/${apiVersion}/recurring_application_charges/${chargeId}/activate.json`, billingBody, {
-                            'X-Shopify-Access-Token': accessToken
-                        })
-                        .then((response) => {
-                            console.log(response.data);
-                        })
-                        .catch((e) => {
-                            console.log(e);
-                        })
-                }
-            })
+            res.status(200).redirect(billing.recurring_application_charge.confirmation_url);
         } else {
             res.status(404).send('Billing data not found!');
         }
--- app/controllers/shopifyController.js
+++ app/controllers/shopifyController.js
@@ -158,7 +158,7 @@ router.get('/callback', (req, res) => {
                                 "recurring_application_charge": {
                                     "name": "Plan@4.99",
                                     "price": 4.99,
-                                    "return_url": "https://e54d0a28.ngrok.io/billings/id",
+                                    "return_url": `${forwardingAddress}/billings/id`,
                                     "test": true
                                 }
                             }, { headers: shopRequestHeaders })
